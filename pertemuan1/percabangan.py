ffe8d14bdcac26ceb44132dfcc51e33d7a5a934e
unknown <bagusasensio20@gmail.com>
2022-12-01 09:59:49 +0700

    pertemuan1

---
+++ pertemuan1/percabangan.py
@@ -0,0 +1,8 @@
+num = eval(input("Masukkan angka : "))
+if num > 0:
+    print("Bilangan Positif")
+elif num < 0 :
+    print("Bilangan Negative")
+else:
+    print("Bukan bilangan positif atau pun negatif")
+print("Selesai")
---
+++ pertemuan1/variable.py
@@ -0,0 +1,8 @@
+nama = "Bagus" # string
+umur = 19 # integer
+isStudent = True # boolean
+ip = 3.50 # float
+
+Umur20thnMendatang = umur + 20 # Oprasi aritmatika hanya dapat dilakukan dengan numeric (int/float)
+
+print("Nama Saya", nama, '. Umur saya 20 tahun mendatang adalah', Umur20thnMendatang)
